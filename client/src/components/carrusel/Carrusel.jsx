e89873a22e5e5ce12e77c436924d5369ef93da5b
jhonshua <julio.llinas@gmail.com>
2023-01-20 22:00:41 -0400

    5 commit

--- client/src/components/carrusel/Carrusel.jsx
+++ client/src/components/carrusel/Carrusel.jsx
@@ -25,42 +25,42 @@ const Carrusel = () => {
   <div class="items">
     <div class="entry">
       <p class="name">Gluten Free</p>
-      <a href="http://localhost:3000/recipe/716426"><img src="https://spoonacular.com/recipeImages/716426-312x231.jpg" alt='no found' /></a>
+      <a href="https://deploy-production-8341.up.railway.app/recipe/716426"><img src="https://spoonacular.com/recipeImages/716426-312x231.jpg" alt='no found' /></a>
       <p class="quote">Cauliflower and Vegetable Fried Rice</p>
     </div>
     <div class="entry">
       <p class="name">Dairy free</p>
-      <a href="http://localhost:3000/recipe/716381"><img src="https://spoonacular.com/recipeImages/716381-312x231.jpg" alt='no found' /></a>
+      <a href="https://deploy-production-8341.up.railway.app/recipe/716381"><img src="https://spoonacular.com/recipeImages/716381-312x231.jpg" alt='no found' /></a>
       <p class="quote">Nigerian Snail Stew</p>
     </div>
     <div class="entry">
       <p class="name">Vegan</p>
-      <a href="http://localhost:3000/recipe/782601"><img src="https://spoonacular.com/recipeImages/782601-312x231.jpg" alt='no found' /></a>
+      <a href="https://deploy-production-8341.up.railway.app/recipe/782601"><img src="https://spoonacular.com/recipeImages/782601-312x231.jpg" alt='no found' /></a>
       <p class="quote">"Red Kidney Bean Jambalaya"</p>
     </div>
     <div class="entry">
       <p class="name">Lacto ovo vegetarian</p>
-      <a href="http://localhost:3000/recipe/715540"><img src="https://spoonacular.com/recipeImages/715540-312x231.jpg" alt='no found' /></a>
+      <a href="https://deploy-production-8341.up.railway.app/recipe/715540"><img src="https://spoonacular.com/recipeImages/715540-312x231.jpg" alt='no found' /></a>
       <p class="quote">"Summer Berry Salad"</p>
     </div>
     <div class="entry">
       <p class="name">Ketogenic</p>
-      <a href="http://localhost:3000/recipe/794538"><img src="https://spoonacular.com/recipeImages/794538-312x231.jpg" alt='no found' /></a>
+      <a href="https://deploy-production-8341.up.railway.app/recipe/794538"><img src="https://spoonacular.com/recipeImages/794538-312x231.jpg" alt='no found' /></a>
       <p class="quote">"Almond Joy Protein Shake"</p>
     </div>
     <div class="entry">
       <p class="name">Peleolithic</p>
-      <a href="http://localhost:3000/recipe/769774"><img src="https://spoonacular.com/recipeImages/769774-312x231.jpg"  alt='no found' /></a>
+      <a href="https://deploy-production-8341.up.railway.app/recipe/769774"><img src="https://spoonacular.com/recipeImages/769774-312x231.jpg"  alt='no found' /></a>
       <p class="quote">"Shredded Roast Beef"</p>
     </div>
     <div class="entry">
       <p class="name">Pescatarian</p>
-      <a href="http://localhost:3000/recipe/639411"><img src="https://spoonacular.com/recipeImages/639411-312x231.jpg" alt='no found'/></a>
+      <a href="https://deploy-production-8341.up.railway.app/recipe/639411"><img src="https://spoonacular.com/recipeImages/639411-312x231.jpg" alt='no found'/></a>
       <p class="quote">"Cilantro Lime Halibut"</p>
     </div>
     <div class="entry">
       <p class="name">Primal</p>
-      <a href="http://localhost:3000/recipe/633344"><img src="https://spoonacular.com/recipeImages/633344-312x231.jpg" alt='no found'/></a>
+      <a href="https://deploy-production-8341.up.railway.app/recipe/633344"><img src="https://spoonacular.com/recipeImages/633344-312x231.jpg" alt='no found'/></a>
       <p class="quote">"Bacon Wrapped Pork"</p>
     </div>
   </div>
